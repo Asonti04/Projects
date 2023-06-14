edd968e5ce4c99389bd6ac90d588f5093838eb87
Asonti <asontidg@gmail.com>
2023-06-14 00:29:20 +0000

    scan & query table

---
+++ Boto3-Dynamo/scan-query.py
@@ -0,0 +1,20 @@
+#SCAN & QUERY
+import boto3
+ddb = boto3.client('dynamodb')
+response = ddb.scan(
+    TableName='Video_Game',
+)
+
+print(response)
+
+response2 = ddb.query(
+    TableName='Video_Game',
+    ExpressionAttributeValues={
+        ':Game': {
+            'S': 'Sims4',
+        },
+    },
+    KeyConditionExpression='Game = :Game'
+)
+
+print(response2)
